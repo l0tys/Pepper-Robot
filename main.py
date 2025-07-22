39a6218c3b1e50f385202ee87bebeec6f9be248d
Patriks Raiss-Reiss <64326465+SeetZy@users.noreply.github.com>
2025-07-22 14:21:58 +0300

    edit

--- main.py
+++ main.py
@@ -2,33 +2,45 @@
 import os
 from dotenv import load_dotenv
 import naoqi
-import time
 
 from actions import wake_up
 
-def main():
-    load_dotenv()
-    PEPPER_IP = os.getenv("PEPPER_IP")
-    PORT = int(os.getenv("PEPPER_PORT"))
+load_dotenv()
 
-    try:
-        # Get all services
-        motion_service = naoqi.ALProxy("ALMotion", PEPPER_IP, PORT)
-        tts_service = naoqi.ALProxy("ALTextToSpeech", PEPPER_IP, PORT)
-        led_service = naoqi.ALProxy("ALLeds", PEPPER_IP, PORT)
-        print("Connected to Pepper robot")
-    except Exception as e:
-        print("Can't connect to Pepper robot: {}".format(e))
-        return
+class Pepper:
+    def __init__(self):
+        self.PEPPER_IP = os.getenv("PEPPER_IP")
+        self.PORT = int(os.getenv("PEPPER_PORT"))
+        self.motion_service = None
+        self.tts_service = None
+        self.led_service = None
+        self.nav_service = None
 
-    try:
-        wake_up(motion_service, led_service, tts_service)
-    except Exception as e:
-        print("An error occurred: {}".format(e))
+    def get_services(self):
+        self.motion_service = naoqi.ALProxy("ALMotion", self.PEPPER_IP, self.PORT)
+        self.tts_service = naoqi.ALProxy("ALTextToSpeech", self.PEPPER_IP, self.PORT)
+        self.led_service = naoqi.ALProxy("ALLeds", self.PEPPER_IP, self.PORT)
+        self.nav_service = naoqi.ALProxy("ALNavigation", self.PEPPER_IP, self.PORT)
 
-    finally:
-        time.sleep(3)
-        motion_service.rest()
+    def main(self):
+        try:
+            self.get_services()
+            print("Connected to Pepper robot")
+        except Exception as e:
+            print("Can't connect to Pepper robot: {}".format(e))
+            return
+
+        try:
+            wake_up(self.motion_service, self.led_service, self.tts_service)
+
+            while True:
+                self.nav_service.explore(5)
+        except Exception as e:
+            print("An error occurred: {}".format(e))
+
+        finally:
+            self.motion_service.rest()
 
 if __name__ == "__main__":
-    main()
+    pepper = Pepper()
+    pepper.main()
